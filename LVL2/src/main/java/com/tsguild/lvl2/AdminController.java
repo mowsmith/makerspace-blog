3dd1d7b15cfd0758b9d4b467a0636db07c6d298c
Morgan Smith <mowsmith@gmail.com>
2016-12-01 13:59:27 -0500

    updated with employee tools and set-up for status codes

--- LVL2/src/main/java/com/tsguild/lvl2/AdminController.java
+++ LVL2/src/main/java/com/tsguild/lvl2/AdminController.java
@@ -1,28 +1,4 @@
-/*
- * Copyright (c) 2016, Dan Whitlow
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+
 package com.tsguild.lvl2;
 
 import com.tsguild.lvl2.dao.BlogPostDao;
@@ -42,10 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- *
- * @author Dan Whitlow
- */
 @Controller
 public class AdminController {
 
@@ -60,7 +32,7 @@ public class AdminController {
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String admin(Model model) {
-        
+
         return "admin";
     }
 
@@ -74,24 +46,45 @@ public class AdminController {
 //    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/blog", method = RequestMethod.POST)
     public BlogPost createBlogPost(@RequestBody BlogPost blogPost, HttpServletRequest request) {
-        if (request.isUserInRole("ROLE_EMPLOYEE")) {
-            blogPost.setStatus(2);
-        } else if (request.isUserInRole("ROLE_ADMIN")) {
-            blogPost.setStatus(1);
+        /*
+        1 Admin Post
+        2 User Post Pending
+        3 User Post Approved
+        4 User Post Declined
+        5 Admin Comment
+        6 Reader comment Pending
+        7 Reader Comment Approved
+        8 Reader Comment Declined
+        9 Draft
+        10 Pending Deletion
+        11 Deleted
+         */
+
+        int s = blogPost.getStatus();
+
+        // admin only status
+        if (s == -1 || s == 1 || s == 2 || s == 3 || s == 4 || s == 9 || s == 10 || s == 11) {
+            if (s == -1 || s == 2) { //if its an unsorted or user post
+                if (request.isUserInRole("ROLE_EMPLOYEE")) {
+                    blogPost.setStatus(2);
+                } else if (request.isUserInRole("ROLE_ADMIN")) {
+                    blogPost.setStatus(1);
+                }
+            } else if (s == 9) { //if its a draft make sure they're authenticated
+                if (!(request.isUserInRole("ROLE_EMPLOYEE") || request.isUserInRole("ROLE_ADMIN"))) {
+                    return new BlogPost();
+                }
+            } else if (!request.isUserInRole("ROLE_ADMIN")) {
+                return new BlogPost();
+            }
+        } else {
+            return new BlogPost();
         }
 
         blogPost.setAuthor(request.getUserPrincipal().getName());
         return blogDao.addBlogPost(blogPost);
     }
-    
-    @ResponseBody
-    @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(value="/blog", method=RequestMethod.PUT)
-    public BlogPost draftBlogPost(@RequestBody BlogPost blogPost){
-        blogPost.setStatus(9);
-        return blogDao.addBlogPost(blogPost);
-    }
-    
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
     public void editBlogPost(@RequestBody BlogPost editedPost, HttpServletRequest request) {
--- LVL2/src/main/java/com/tsguild/lvl2/BlogPostController.java
+++ LVL2/src/main/java/com/tsguild/lvl2/BlogPostController.java
@@ -11,10 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- *
- * @author Morgan Smith
- */
 @Controller
 public class BlogPostController {
 
